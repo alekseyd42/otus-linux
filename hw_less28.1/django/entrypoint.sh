46cbf462ccf4550c3e475af14f767a31ff1346cc
alekseyd42 <dubinskiy@mail.ru>
2020-03-11 14:07:14 +0300

    mssql-cluster

--- hw_less28.1/django/entrypoint.sh
+++ hw_less28.1/django/entrypoint.sh
@@ -2,5 +2,6 @@
 if [ ! -d "/www/djtst" ]
 then 
 django-admin.py startproject djtst /www
+sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \['*'\]/" /www/djtst/settings.py
 fi
 uwsgi --socket :9090 --plugin python3 --chdir /www/ --wsgi-file djtst/wsgi.py --ini /etc/uwsgi/uwsgi.ini
